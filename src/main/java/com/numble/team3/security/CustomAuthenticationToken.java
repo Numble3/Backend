ac934d491d3e9fffb625829707a0a4dbfa02ea24
apptie <appti@naver.com>
2022-04-23 02:17:23 +0900

    [feat] #3 Spring Security 기본 설정 추가

---
+++ src/main/java/com/numble/team3/security/CustomAuthenticationToken.java
@@ -0,0 +1,27 @@
+package com.numble.team3.security;
+
+import java.util.Collection;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+
+public class CustomAuthenticationToken extends AbstractAuthenticationToken {
+  private CustomUserDetails principal;
+
+  public CustomAuthenticationToken(
+      CustomUserDetails principal,
+      Collection<? extends GrantedAuthority> authorities) {
+    super(authorities);
+    this.principal = principal;
+    setAuthenticated(true);
+  }
+
+  @Override
+  public Object getCredentials() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public Object getPrincipal() {
+    return principal;
+  }
+}
