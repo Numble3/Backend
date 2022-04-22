fae39aee618d8554a33664549bfd6e3422f61656
apptie <appti@naver.com>
2022-04-23 01:38:55 +0900

    [feat] #3 Spring Security 기본 설정 추가

---
+++ src/main/java/com/numble/team3/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.numble.team3.config;
+
+import com.numble.team3.jwt.TokenHelper;
+import com.numble.team3.security.CustomAccessDeniedHandler;
+import com.numble.team3.security.CustomAuthenticationEntryPoint;
+import com.numble.team3.security.CustomOAuth2UserService;
+import com.numble.team3.security.CustomUserDetailsService;
+import com.numble.team3.security.JwtAuthenticationFilter;
+import com.numble.team3.security.OAuth2SuccessHandler;
+import com.numble.team3.sign.infra.RedisUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  private final CustomUserDetailsService userDetailsService;
+  private final RedisUtils redisUtils;
+  private final TokenHelper accessTokenHelper;
+  private final CustomOAuth2UserService oAuth2UserService;
+  private final OAuth2SuccessHandler oAuth2SuccessHandler;
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+      .httpBasic().disable()
+      .formLogin().disable()
+      .csrf().disable()
+      .sessionManagement()
+      .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+      .and()
+      .authorizeRequests()
+      .antMatchers("/favicon.ico", "/profile").permitAll()
+      .anyRequest().authenticated()
+
+      .and()
+      .exceptionHandling().accessDeniedHandler(new CustomAccessDeniedHandler())
+      .and()
+      .exceptionHandling().authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+      .and()
+      .addFilterBefore(
+        new JwtAuthenticationFilter(userDetailsService, redisUtils, accessTokenHelper),
+        UsernamePasswordAuthenticationFilter.class);
+  }
+}
---
+++ src/main/java/com/numble/team3/security/JwtAuthenticationFilter.java
@@ -0,0 +1,40 @@
+package com.numble.team3.security;
+
+import com.numble.team3.jwt.TokenHelper;
+import com.numble.team3.sign.infra.RedisUtils;
+import java.io.IOException;
+import java.util.Optional;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends GenericFilterBean {
+
+  private final CustomUserDetailsService userDetailsService;
+  private final RedisUtils redisUtils;
+  private final TokenHelper accessTokenHelper;
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+    throws IOException, ServletException {
+      extractToken(request).filter(token -> redisUtils.validToken(token, "accessToken", Optional.ofNullable(
+          accessTokenHelper.parse(token).map(claims -> claims.getAccountId()).orElse(null))))
+        .map(userDetailsService::loadUserByUsername).ifPresent(this::setAuthentication);
+      chain.doFilter(request, response);
+  }
+
+  private void setAuthentication(CustomUserDetails userDetails) {
+    SecurityContextHolder.getContext()
+      .setAuthentication(new CustomAuthenticationToken(userDetails, userDetails.getAuthorities()));
+  }
+
+  private Optional<String> extractToken(ServletRequest request) {
+    return Optional.ofNullable(((HttpServletRequest) request).getHeader("Authorization"));
+  }
+}
