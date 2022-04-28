e04790259d813552a4e11e0aa36aa170058a9501
arsgsg1 <arsgsg1@gmail.com>
2022-04-28 16:28:10 +0900

    Video 컨트롤러 패키지 이동

--- src/main/java/com/numble/team3/video/controller/VideoController.java
+++ src/main/java/com/numble/team3/video/controller/VideoController.java
@@ -1,18 +1,13 @@
-package com.numble.team3.video.application.controller;
+package com.numble.team3.video.controller;
 
 import com.numble.team3.account.annotation.LoginUser;
 import com.numble.team3.account.resolver.UserInfo;
-import com.numble.team3.video.application.VideoService;
-import com.numble.team3.video.application.request.CreateVideoDto;
-import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
