64d9d9db00282644fda148a5dde4c2c12f5901ef
arsgsg1 <arsgsg1@gmail.com>
2022-05-03 11:17:53 +0900

    [feat] #31 테스트 시 Mock Bean을 등록하도록 수정
    테스트할 땐 Mock객체로 대신 넣어도 무방함

--- src/test/java/com/numble/team3/Team3ApplicationTests.java
+++ src/test/java/com/numble/team3/Team3ApplicationTests.java
@@ -1,10 +1,15 @@
 package com.numble.team3;
 
+import com.numble.team3.video.domain.VideoUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class Team3ApplicationTests {
+
+	@MockBean
+	protected VideoUtils videoUtils;
 
 	@Test
 	void contextLoads() {
