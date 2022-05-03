74a376b60b82aaa14d12c95cb785a44a599f8228
apptie <appti@naver.com>
2022-05-03 12:24:40 +0900

    [docs] #40 Image Swagger 추가

--- src/main/java/com/numble/team3/image/application/request/CreateImageDto.java
+++ src/main/java/com/numble/team3/image/application/request/CreateImageDto.java
@@ -1,5 +1,6 @@
 package com.numble.team3.image.application.request;
 
+import io.swagger.annotations.ApiModelProperty;
 import javax.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,15 +10,19 @@ import org.springframework.web.multipart.MultipartFile;
 @Setter
 public class CreateImageDto {
 
+  @ApiModelProperty(value = "이미지 파일", required = true)
   @NotBlank(message = "업로드할 이미지 파일을 선택해주세요.")
   private MultipartFile file;
 
+  @ApiModelProperty(value = "리사이즈 가로 길이", required = true)
   @NotBlank(message = "리사이즈할 가로 길이를 입력해주세요.")
   private String width;
 
+  @ApiModelProperty(value = "리사이즈 세로 길이", required = true)
   @NotBlank(message = "라사이즈할 세로 길이를 입력해주세요.")
   private String height;
 
+  @ApiModelProperty(value = "리사이즈 타입(thumbnail / profile)", required = true)
   @NotBlank(message = "리사이즈할 타입을 입력해주세요.")
   private String type;
 }
--- src/main/java/com/numble/team3/image/controller/ImageController.java
+++ src/main/java/com/numble/team3/image/controller/ImageController.java
@@ -1,8 +1,10 @@
 package com.numble.team3.image.controller;
 
 import com.numble.team3.exception.image.ImageResizeTypeUnSupportException;
+import com.numble.team3.image.annotation.ImageResizeSwagger;
 import com.numble.team3.image.application.ImageService;
 import com.numble.team3.image.application.request.CreateImageDto;
+import io.swagger.annotations.Api;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,10 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
+@Api(tags = {"이미지 리사이즈"})
 public class ImageController {
 
   private final ImageService imageService;
 
+  @ImageResizeSwagger
   @PostMapping(value = "/images/resize", produces = "application/json")
   public ResponseEntity<Map> imageResize(@ModelAttribute CreateImageDto dto) throws IOException {
     if (!(dto.getType().equals("profile") || dto.getType().equals("thumbnail"))) {
