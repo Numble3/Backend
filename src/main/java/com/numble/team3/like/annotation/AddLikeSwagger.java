29de90e1051778f0f54dbc087bfdac07db17a3e8
apptie <appti@naver.com>
2022-05-04 12:48:50 +0900

    [docs] #40 Like Swagger 추가

---
+++ src/main/java/com/numble/team3/like/annotation/AddLikeSwagger.java
@@ -0,0 +1,53 @@
+package com.numble.team3.like.annotation;
+
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.Example;
+import io.swagger.annotations.ExampleProperty;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@ApiOperation(value = "좋아요 추가")
+@ApiResponses(
+  value = {
+    @ApiResponse(
+      code = 201,
+      message = "좋아요 추가 성공",
+      examples = @Example(@ExampleProperty(mediaType = "application/json", value = "{}"))
+    ),
+    @ApiResponse(
+      code = 400,
+      message = "좋아요 추가 실패 \t\n 1. 존재하지 않는 비디오 \t\n 2. 쿼리 파라미터가 존재하지 않음 \t\n 3. 쿼리 파라미터의 타입이 올바르지 않음",
+      examples = @Example(@ExampleProperty(mediaType = "application/json", value = "{\n\"message\" : \"존재하지 않는 비디오입니다.\"\n\"message\" : \"쿼리 파라미터를 확인해주세요.\"\n\"message\" : \"쿼리 파라미터의 타입을 확인해주세요.\"\n}"))
+    ),
+    @ApiResponse(
+      code = 401,
+      message = "좋아요 추가 실패 \t\n 1. access token이 유효하지 않음",
+      examples = @Example(@ExampleProperty(mediaType = "application/json", value = "{}"))
+    )
+  }
+)
+@ApiImplicitParams(
+  value = {
+    @ApiImplicitParam(
+      name = "Authorization",
+      value = "access token",
+      required = true,
+      dataType = "String",
+      paramType = "header"
+    ),
+  }
+)
+@ResponseStatus(HttpStatus.CREATED)
+public @interface AddLikeSwagger {
+
+}
--- src/main/java/com/numble/team3/like/controller/LikeController.java
+++ src/main/java/com/numble/team3/like/controller/LikeController.java
@@ -2,9 +2,15 @@ package com.numble.team3.like.controller;
 
 import com.numble.team3.account.annotation.LoginUser;
 import com.numble.team3.account.resolver.UserInfo;
+import com.numble.team3.like.annotation.AddLikeSwagger;
+import com.numble.team3.like.annotation.DeleteLikeSwagger;
+import com.numble.team3.like.annotation.GetAllLikesSwagger;
+import com.numble.team3.like.annotation.GetLikesByCategorySwagger;
+import com.numble.team3.like.annotation.GetRankByDaySwagger;
 import com.numble.team3.like.application.LikeService;
 import com.numble.team3.like.application.response.GetAllLikeListDto;
 import com.numble.team3.like.application.response.GetVideoRankDto;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiParam;
 import java.util.HashMap;
 import java.util.List;
@@ -23,39 +29,44 @@ import springfox.documentation.annotations.ApiIgnore;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
+@Api(tags = {"관심영상 조회, 랭킹, 좋아요 추가, 좋아요 삭제"})
 public class LikeController {
 
   private final LikeService likeService;
 
+  @AddLikeSwagger
   @PostMapping(value = "/likes/add", produces = "application/json")
   public ResponseEntity addLike(
-    @LoginUser UserInfo userInfo,
-    @RequestParam(name = "id") Long videoId) {
+    @ApiIgnore @LoginUser UserInfo userInfo,
+    @ApiParam(value = "비디오 id", required = true) @RequestParam(name = "id") Long videoId) {
       likeService.addLike(userInfo, videoId);
       return new ResponseEntity(HttpStatus.CREATED);
   }
 
+  @DeleteLikeSwagger
   @DeleteMapping(value = "/likes/delete", produces = "application/json")
-  public ResponseEntity deleteLike(@RequestParam(name = "id") Long likeId) {
+  public ResponseEntity deleteLike(@ApiParam(value = "좋아요 id", required = true) @RequestParam(name = "id") Long likeId) {
     likeService.deleteLike(likeId);
     return new ResponseEntity(HttpStatus.OK);
   }
 
+  @GetAllLikesSwagger
   @GetMapping(value = "/likes/all", produces = "application/json")
-  public ResponseEntity<GetAllLikeListDto> getLikesHierarchy(
-    @LoginUser UserInfo userInfo) {
+  public ResponseEntity<GetAllLikeListDto> getLikesHierarchy(@ApiIgnore @LoginUser UserInfo userInfo) {
     return ResponseEntity.ok(likeService.getLikesHierarchy(userInfo));
   }
 
+  @GetLikesByCategorySwagger
   @GetMapping(value = "/likes", produces = "application/json")
   public ResponseEntity getLikesByCategory(
-    @LoginUser UserInfo userInfo,
-    @RequestParam(name = "category") String categoryName,
-    @RequestParam(required = false, name = "id") Long likeId,
-    @RequestParam(name = "size") int size) {
+    @ApiIgnore @LoginUser UserInfo userInfo,
+    @ApiParam(value = "카테고리 이름", required = true) @RequestParam(name = "category") String categoryName,
+    @ApiParam(value = "like id, 2페이지 이후 조회를 위한 값", required = false) @RequestParam(required = false, name = "id") Long likeId,
+    @ApiParam(value = "페이지 크기", required = true) @RequestParam(name = "size") int size) {
       return ResponseEntity.ok(likeService.getLikesByCategory(userInfo, categoryName, likeId, size));
   }
 
+  @GetRankByDaySwagger
   @GetMapping(value = "/likes/rank/day", produces = "application/json")
   public ResponseEntity<Map> getRankByDay() {
     return new ResponseEntity(new HashMap<String, List<GetVideoRankDto>>() {
