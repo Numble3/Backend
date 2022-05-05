d9d819dbcce7df6aa29c922081efcfd663e70d4b
apptie <appti@naver.com>
2022-05-05 15:16:25 +0900

    [refactor] #57 likeService Redis 의존성 분리

---
+++ src/main/java/com/numble/team3/like/domain/LikeUtils.java
@@ -0,0 +1,11 @@
+package com.numble.team3.like.domain;
+
+import com.numble.team3.like.application.response.GetVideoRankDto;
+import java.util.List;
+
+public interface LikeUtils {
+
+  void processChangeDayRanking(String standard, List<GetVideoRankDto> ranking);
+
+  List<GetVideoRankDto> getDayRanking(String standard);
+}
