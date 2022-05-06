5b60cd478bcd7d9ad8262cf1e9ec4dfe0f504d03
apptie <appti@naver.com>
2022-05-07 00:47:44 +0900

    [docs] #40 Account Swagger 추가

--- src/main/java/com/numble/team3/account/application/request/UpdateMyAccountDto.java
+++ src/main/java/com/numble/team3/account/application/request/UpdateMyAccountDto.java
@@ -1,13 +1,16 @@
 package com.numble.team3.account.application.request;
 
+import io.swagger.annotations.ApiModelProperty;
 import javax.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class UpdateMyAccountDto {
 
+  @ApiModelProperty(value = "profile url 경로", required = false)
   private String profile;
 
+  @ApiModelProperty(value = "닉네임", required = true)
   @NotBlank(message = "닉네임을 입력해주세요.")
   private String nickname;
 }
