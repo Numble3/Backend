d7813436e83d98e73a367415ee0aaf148ce0fcbd
apptie <appti@naver.com>
2022-05-06 22:24:32 +0900

    [docs] #40 관리자 회원관리 Swagger 추가

---
+++ src/main/java/com/numble/team3/admin/annotation/GetAccountSwagger.java
@@ -0,0 +1,42 @@
+package com.numble.team3.admin.annotation;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.Example;
+import io.swagger.annotations.ExampleProperty;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@ApiOperation(value = "회원 단일 조회")
+@ApiResponses(
+  value = {
+    @ApiResponse(
+      code = 200,
+      message = "회원 단일 조회 성공",
+      examples = @Example(@ExampleProperty(mediaType = "application/json", value = "{\n\"id\" : account id, \n\"email\" : \"email\", \n\"nickname\" : \"nickname\", \n\"lastLogin\" : \"yyyy-MM-dd\", \n\"createdAt\" : \"yyyy-MM-dd\" \n}"))
+    ),
+    @ApiResponse(
+      code = 400,
+      message = "회원 단일 조회 실패 \t\n 1. 존재하지 않는 회원",
+      examples = @Example(@ExampleProperty(mediaType= "application/json", value = "{\"message\" : \"존재하지 않는 회원입니다.\"}"))
+    ),
+    @ApiResponse(
+      code = 401,
+      message = "회원 단일 조회 실패 \t\n 1. access token이 유효하지 않음",
+      examples = @Example(@ExampleProperty(mediaType= "application/json", value = "{}"))
+    ),
+    @ApiResponse(
+      code = 403,
+      message = "회원 단일 조회 실패 \t\n 1. 권한 없음",
+      examples = @Example(@ExampleProperty(mediaType= "application/json", value = "{}"))
+    ),
+  }
+)
+public @interface GetAccountSwagger {
+
+}
--- src/main/java/com/numble/team3/admin/controller/AdminAccountController.java
+++ src/main/java/com/numble/team3/admin/controller/AdminAccountController.java
@@ -1,7 +1,13 @@
 package com.numble.team3.admin.controller;
 
 import com.numble.team3.account.application.AccountService;
+import com.numble.team3.admin.annotation.DeleteTokenSwagger;
+import com.numble.team3.admin.annotation.GetAccountSwagger;
+import com.numble.team3.admin.annotation.GetAccountsSwagger;
+import com.numble.team3.admin.annotation.WithdrawalSwagger;
 import com.numble.team3.sign.application.SignService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
@@ -17,25 +23,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/admin")
+@Api(tags = {"회원 관리"})
 public class AdminAccountController {
 
   private final AccountService accountService;
   private final SignService signService;
 
+  @GetAccountsSwagger
   @GetMapping(value = "/accounts/all", produces = "application/json")
   @PreAuthorize("hasRole('ROLE_ADMIN')")
   public ResponseEntity getAccounts(
-    @RequestParam("page") int page,
-    @RequestParam("size") int size) {
+    @ApiParam(value = "페이지", required = true) @RequestParam("page") int page,
+    @ApiParam(value = "페이지 크기", required = true) @RequestParam("size") int size) {
     return ResponseEntity.ok(accountService.getAccounts(PageRequest.of(page - 1, size)));
   }
 
+  @GetAccountSwagger
   @GetMapping(value = "/accounts/{id}", produces = "application/json")
   @PreAuthorize("hasRole('ROLE_ADMIN')")
   public ResponseEntity getAccount(@PathVariable Long id) {
     return ResponseEntity.ok(accountService.getAccountByAdmin(id));
   }
 
+  @WithdrawalSwagger
   @DeleteMapping(value = "/accounts/withdrawal/{id}", produces = "application/json")
   @PreAuthorize("hasRole('ROLE_ADMIN')")
   public ResponseEntity withdrawalAccount(@PathVariable Long id) {
@@ -43,6 +53,7 @@ public class AdminAccountController {
     return new ResponseEntity(HttpStatus.OK);
   }
 
+  @DeleteTokenSwagger
   @DeleteMapping(value = "/accounts/access-token/{id}", produces = "application/json")
   @PreAuthorize("hasRole('ROLE_ADMIN')")
   public ResponseEntity deleteAccessToken(@PathVariable Long id) {
