a1dd3165f6300e0b7125ffd69c0cdaa28ad60d5b
apptie <appti@naver.com>
2022-05-08 18:52:40 +0900

    [refactor] CreateImageDto 수정

--- src/main/java/com/numble/team3/converter/application/request/CreateImageDto.java
+++ src/main/java/com/numble/team3/converter/application/request/CreateImageDto.java
@@ -2,6 +2,7 @@ package com.numble.team3.converter.application.request;
 
 import io.swagger.annotations.ApiModelProperty;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,7 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class CreateImageDto {
 
   @ApiModelProperty(value = "이미지 파일", required = true)
-  @NotBlank(message = "업로드할 이미지 파일을 선택해주세요.")
+  @NotNull(message = "업로드할 이미지 파일을 선택해주세요.")
   private MultipartFile file;
 
   @ApiModelProperty(value = "리사이즈 가로 길이", required = true)
@@ -19,7 +20,7 @@ public class CreateImageDto {
   private String width;
 
   @ApiModelProperty(value = "리사이즈 세로 길이", required = true)
-  @NotBlank(message = "라사이즈할 세로 길이를 입력해주세요.")
+  @NotBlank(message = "리사이즈할 세로 길이를 입력해주세요.")
   private String height;
 
   @ApiModelProperty(value = "리사이즈 타입(thumbnail / profile)", required = true)
