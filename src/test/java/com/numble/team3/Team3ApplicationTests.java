a571ab229d5eb3e8d1805cc9b82e33f954542771
arsgsg1 <arsgsg1@gmail.com>
2022-05-13 00:47:51 +0900

    [feat] #86 테스트 통과를 위한 MockBean 추가

--- src/test/java/com/numble/team3/Team3ApplicationTests.java
+++ src/test/java/com/numble/team3/Team3ApplicationTests.java
@@ -1,11 +1,15 @@
 package com.numble.team3;
 
+import com.numble.team3.converter.domain.ConvertVideoUtils;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class Team3ApplicationTests {
+
+  @MockBean
+  protected ConvertVideoUtils convertVideoUtils;
 
   @Test
   void contextLoads() {}
