08968849e8a92263e22467baf7ea47efc5c439cb
arsgsg1 <arsgsg1@gmail.com>
2022-05-12 15:25:34 +0900

    [feat] #82 url 수정

--- src/main/java/com/numble/team3/admin/controller/AdminVideoController.java
+++ src/main/java/com/numble/team3/admin/controller/AdminVideoController.java
@@ -12,14 +12,13 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/admin")
 public class AdminVideoController {
   private final VideoService videoService;
 
