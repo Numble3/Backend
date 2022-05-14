4b45ce203ea58455e74e03d5524d4a4891e56b94
arsgsg1 <arsgsg1@gmail.com>
2022-05-14 13:39:31 +0900

    [docs] #132 댓글 조회 스웨거 문서 추가

--- src/main/java/com/numble/team3/comment/application/response/GetCommentListDto.java
+++ src/main/java/com/numble/team3/comment/application/response/GetCommentListDto.java
@@ -1,6 +1,7 @@
 package com.numble.team3.comment.application.response;
 
 import com.numble.team3.comment.domain.Comment;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.Builder;
@@ -11,8 +12,13 @@ import org.springframework.data.domain.Slice;
 @Getter
 @NoArgsConstructor
 public class GetCommentListDto {
+  @Schema(description = "댓글 내용 리스트")
   private List<GetCommentDto> contents;
+
+  @Schema(description = "로그인된 계정이라면, 좋아요를 누른 댓글의 ID 리스트")
   private List<Long> likeComments;
+
+  @Schema(description = "다음 페이지 여부")
   private boolean hasNext;
 
   @Builder
