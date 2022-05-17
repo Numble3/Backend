2be9823aad29df7e632be588b01a966404115dee
apptie <appti@naver.com>
2022-05-17 16:28:22 +0900

    [refactor] GetLikeListDto 수정

--- src/main/java/com/numble/team3/like/application/response/GetLikeListDto.java
+++ src/main/java/com/numble/team3/like/application/response/GetLikeListDto.java
@@ -1,6 +1,5 @@
 package com.numble.team3.like.application.response;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,7 +10,6 @@ public class GetLikeListDto {
 
   private List<GetLikeVideoDto> likes;
 
-  @JsonInclude(JsonInclude.Include.NON_NULL)
   private Long lastLikeId;
 }
 
